94519e54dd446880566dec0e31f39a2b6db37df1
Eric Quanz <eric.quanz@gmail.com>
2016-05-20 11:17:37 -0300

    persistent login and related tests

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
   test "login with invalid validation" do
     get login_path
     assert_template 'sessions/new'
@@ -12,7 +16,6 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   end
 
   test "logs in and out with a valid user" do
-    @user = users(:michael)
     get login_path
     post login_path, session: { email: @user.email, password: 'password' }
     assert is_logged_in?
@@ -25,9 +28,20 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+  test "logging in can remember user" do
+    log_in_as(@user, remember_me: '1')
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
+  end
+
+  test "logging in without remembering" do
+    log_in_as(@user, remember_me: '0')
+    assert_nil cookies['remember_token']
   end
 end

84179e6a3c32a3704d1b986c0f843b54ac48ddbf
Eric Quanz <eric.quanz@gmail.com>
2016-05-24 10:57:48 -0300

    add account activation fields to user model, update fixtures and seeds accordingly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
-  before_save { email.downcase! }
+  before_create :create_activation_digest
+  before_save :sanitize_email
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
@@ -39,4 +40,14 @@ class User < ActiveRecord::Base
   def forget
     update_attribute(:remember_digest, nil);
   end
+
+  private
+    def sanitize_email
+      email.downcase!
+    end
+
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end

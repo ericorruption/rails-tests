f036acdd2b6ba7d95bda600913c87f3bb9444288
Eric Quanz <eric.quanz@gmail.com>
2016-05-24 13:49:17 -0300

    update methods to account for user authentication before logging in

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -14,16 +19,31 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.field_with_errors'
   end
 
-  test "valid signup creates a new user" do
+  test "valid signup and user activation" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post_via_redirect users_path, user: { name: "durval",
+      post users_path, user: { name: "durval",
                                email: "durval@g.co",
                                password: "Dzprav06!",
                                password_confirmation: "Dzprav06!" }
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    # Try to login before activation
+    log_in_as(user)
+    assert_not is_logged_in?
+    # Test invalid activation token
+    get edit_account_activation_path('invalid token')
+    assert_not is_logged_in?
+    # Valid token, wrong email
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+    # Activate user
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
+    follow_redirect!
     assert_template 'users/show'
-    assert_not flash.empty?
     assert is_logged_in?
   end
 end
